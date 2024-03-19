731e046307e0ac57953f9d97791ad8833bbce8d6
Smcktht <marahsckee@yahoo.com>
2024-03-19 19:42:19 -0400

    chessboard that took too long

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const whiteBox = (
+    <div
+      style={{
+        width: "20px",
+        height: "20px",
+        backgroundColor: "white",
+      }}
+    ></div>
+  );
+  const blackBox = (
+    <div
+      style={{
+        width: "20px",
+        height: "20px",
+        backgroundColor: "black",
+      }}
+    ></div>
+  );
+  // const arr = [1, 2, 3];
+  // arr.reverse();
+  // arr --> [3, 2, 1]
+
+  // to check if a number is even or odd:
+  // const num = 4;
+  // num % 2 --> 0, which means even
+  // const newNum = 7;
+  // newNum % 2 --> 1, which means odd
+
+  // boolean --> true or false
+  // 'thing' === 'other thing' --> false
+  // 4 + 2 === 6 --> true
+
+  // if statement, only runs if the thing inside it is true
+  // if (thing) {
+  //   // if thing is true, then this code will run
+  // } else {
+  //   // if thing is fale, this code will run
+  // }
+
+  const row = (colorToStartWith) => {
+    const output = [];
+    for (let i = 0; i < 4; i++) {
+      if (colorToStartWith === "white") {
+        output.push(whiteBox);
+        output.push(blackBox);
+      } else {
+        output.push(blackBox);
+        output.push(whiteBox);
+      }
+    }
+    return (
+      <div style={{ display: "flex", flexDirection: "row" }}>{output}</div>
+    );
+  };
+
+  const col = () => {
+    const output = [];
+    for (let i = 0; i < 4; i++) {
+      output.push(row("white"));
+      output.push(row("black"));
+    }
+    return (
+      <div style={{ display: "flex", flexDirection: "column" }}>{output}</div>
+    );
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">{col()}</header>
     </div>
   );
 }
